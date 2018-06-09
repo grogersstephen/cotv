09103cc859cf5df17f818315fc3eeb1c29654c90
grogersstephen <gradysrogers@lionmail.sagu.edu>
2018-06-09 16:10:17 -0400

    Create servicesdb.js

---
+++ servicesdb.js
@@ -0,0 +1,22 @@
+var services = [
+{
+	title: "Galatians pt 4",
+	series: "Galatians",
+	speaker: "Tyler Crowder",
+	date: "June 10, 2018",
+	video: "",
+	image: "https://s33.postimg.cc/sruakb2rz/061118_Video_Title_Cover.jpg",
+	wtime: "",
+	mtime: ""
+  },
+  {
+	title: "Galatians pt 3",
+	series: "Galatians",
+	speaker: "Pastor Dave Hobby",
+	date: "June 03, 2018",
+	video: "https://www.youtube.com/embed/eHNPAdFLiYw?amp;showinfo=0&rel=0",
+	image: "https://s33.postimg.cc/gnst8wozz/060418_Video_Title_Cover.jpg",
+	wtime: "https://www.youtube.com/embed/eHNPAdFLiYw?start=318&showinfo=0&rel=0&autoplay=1",
+	mtime: "https://www.youtube.com/embed/eHNPAdFLiYw?start=1608&showinfo=0&rel=0&autoplay=1"
+  }
+  ]
